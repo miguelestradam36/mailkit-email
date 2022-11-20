65109ed1440b3f85c21bd12134604cc7c60d9a45
Miguel Estrada <miguelestradam36@estudiantec.cr>
2022-11-20 17:57:41 -0600

    Updating Mailkit program

--- Mailkit.cs
+++ Mailkit.cs
@@ -1,43 +1,64 @@
-﻿using MimeKit.Text;
+﻿using MailKit.Net.Smtp;
+using MailKit.Security;
+using Microsoft.Extensions.Options;
 using MimeKit;
-using MailKit.Net.Smtp;
+using MimeKit.Text;
 
-if (ModelState.IsValid)
+public class EmailMessage
 {
-    try
+	public string ToAddress { get; set; }
+	public string FromAddress { get; set; }
+	public string Subject { get; set; }
+	public string Content { get; set; }
+}
+
+public interface IEmailService
+{
+    void Send(string from, string to, string subject, string html);
+}
+
+public class EmailService : IEmailService
+{
+    private readonly AppSettings _appSettings;
+
+    public EmailService(IOptions<AppSettings> appSettings)
     {
-        string emailBody = string.Empty;
-        //instantiate a new MimeMessage
-        var message = new MimeMessage();
-        //Setting the To e-mail address
-        message.To.Add(MailboxAddress.Parse(SuscribeModel.Email));
-        //Setting the From e-mail address
-        message.From.Add(MailboxAddress.Parse(""));
-        //E-mail subject 
-        message.Subject = "Subscription Confirmation";
-        //E-mail message body
-        emailBody = "<h2>Thanks for subscribing! </h2>" 
-                    + "<h4>Sincerily,<br>Miguel Estrada</h4>";
-        message.Body = new TextPart(TextFormat.Html)
+        _appSettings = appSettings.Value;
+    }
+
+    public void Send(EmailMessage EmailMessage)
+    {
+        try
         {
-            Text = emailBody
-        };
-        //Configure the e-mail
-        using (var emailClient = new SmtpClient())
+            string emailBody = string.Empty;
+            //instantiate a new MimeMessage
+            var message = new MimeMessage();
+            //Setting the To e-mail address
+            message.To.Add(MailboxAddress.Parse(EmailMessage.ToAddress));
+            //Setting the From e-mail address
+            message.From.Add(MailboxAddress.Parse(EmailMessage.FromAddress));
+            //E-mail subject 
+            message.Subject = EmailMessage.Subject;
+            //E-mail message body
+            emailBody = "<h2>Thanks for following! </h2>"
+                        + $"<p>{EmailMessage.Content}</p>"
+                        + "<h4>Sincerily,<br>Miguel Estrada</h4>";
+            message.Body = new TextPart(TextFormat.Html)
+            {
+                Text = emailBody
+            };
+            //Configure the e-mail
+            using (var emailClient = new SmtpClient())
+            {
+                emailClient.Connect("[HOST]", [PORT], true); // The last is security
+                emailClient.Authenticate("[USER]", "[PASS]");
+                emailClient.Send(message);
+                emailClient.Disconnect(true);
+            }
+        }
+        catch (Exception ex)
         {
-            emailClient.Connect("", 465, true);
-            emailClient.Authenticate("", "");
-            emailClient.Send(message);
-            emailClient.Disconnect(true);
+            var error = $"ERROR: {ex}";
         }
     }
-    catch (Exception ex)
-    {
-        ModelState.Clear();
-    }
-
-}
-else
-{
-    ModelState.Clear();
 }
